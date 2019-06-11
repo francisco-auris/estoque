9e76c0a0a7f6496e84d4fca86a751e77870d76f4
Auris maciel <auris.maciel@gmail.com>
2019-06-10 21:16:31 -0300

    Fazendo tela de login

---
+++ estoque/resources/views/partials/header.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <title>Estoque</title>
+  </head>
+  <body>
